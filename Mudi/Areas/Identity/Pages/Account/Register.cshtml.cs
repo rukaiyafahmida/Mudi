8ee9ff423a6f78a0186fbad006cd185b27c6aed5
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-16 14:35:04 +0600

    Admin register bug fixed

--- Mudi/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Mudi/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -129,15 +129,16 @@ namespace Mudi.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        if (!User.IsInRole(WC.AdminRole))
+                        if (User.IsInRole(WC.AdminRole))
                         {
-                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            TempData[WC.Success] = user.FullName + " has been registered";
+                            return RedirectToAction("Index", "Home");
                         }
                         else
                         {
-                            return RedirectToAction("Index");
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                            return LocalRedirect(returnUrl);
                         }
-                        return LocalRedirect(returnUrl);
                     }
                 }
                 foreach (var error in result.Errors)
