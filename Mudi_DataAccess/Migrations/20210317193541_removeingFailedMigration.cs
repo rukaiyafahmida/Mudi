10b4873fe819891246b39ffbdfd6a5888949a2fa
Tanim <42853370+tanim913@users.noreply.github.com>
2021-03-18 01:53:54 +0600

    unnecessary migrations

---
+++ Mudi_DataAccess/Migrations/20210317193541_removeingFailedMigration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mudi_DataAccess.Migrations
+{
+    public partial class removeingFailedMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
