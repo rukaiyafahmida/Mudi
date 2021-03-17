b5aefb4f3d6e77e5dca8e82ea7a6ba6f21f24aa6
rukaiya fahmida <rukaiyafahmida7@gmail.com>
2021-03-17 20:35:00 +0600

    Added ICartRepository

---
+++ Mudi_DataAccess/Repository/IRepository/ICartRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Mudi_DataAccess.Repository.IRepository;
+using Mudi_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mudi_DataAccess.Repository.IRepository
+{
+    public interface ICartRepository : IRepository<WishListDetail>
+    {
+        void Update(WishListDetail obj);
+
+    }
+}
